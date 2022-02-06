97415abc34b6ed4424135b7194a366b8475b9276
Abhilash Tiwari <abhilashkumartiwari.cse20@jecrc.ac.in>
2022-02-07 03:50:14 +0530

    Create dfs-recursive.js

---
+++ DSA/Graphs/dfs/dfs-recursive.js
@@ -0,0 +1,19 @@
+const depthFirstSearch = (graph, source)=>{
+	console.log(source);
+  
+  for(let neighbor of graph[source]){
+  	depthFirstSearch(graph,neighbor);
+  }
+}
+
+
+const graph = {
+  a: ['b', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: ['f'],
+  e: [],
+  f: []
+};
+
+depthFirstSearch(graph,'a'); //abdfce
