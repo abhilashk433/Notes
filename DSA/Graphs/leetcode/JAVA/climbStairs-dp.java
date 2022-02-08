f2dd68246104a21440f9f543fd20a7e06ded4335
Abhilash Tiwari <abhilashkumartiwari.cse20@jecrc.ac.in>
2022-02-09 01:27:44 +0530

    Create climbStairs-dp.java

---
+++ DSA/Graphs/leetcode/JAVA/climbStairs-dp.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    static int solve(int n, int dp[]){      
+        if(n == 0)
+            return 1;
+        if(n<0)
+            return 0;
+        
+        if(dp[n]!= -1){
+            return dp[n];
+        }else{
+            int ans1 = solve(n-1,dp);
+            int ans2 = solve(n-2,dp);
+            dp[n]=ans1+ans2;
+        }
+        
+        return dp[n];
+    }
+}
