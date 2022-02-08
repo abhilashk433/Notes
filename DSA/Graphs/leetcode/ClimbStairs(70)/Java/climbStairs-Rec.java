c08e98846cb17fa8eecbf9cb7fd1fb4add07cf58
Abhilash Tiwari <abhilashkumartiwari.cse20@jecrc.ac.in>
2022-02-09 01:43:22 +0530

    Update climbStairs-Rec.java

--- DSA/Graphs/leetcode/ClimbStairs(70)/Java/climbStairs-Rec.java
+++ DSA/Graphs/leetcode/ClimbStairs(70)/Java/climbStairs-Rec.java
@@ -1,3 +1,4 @@
+//Recursive solution below
 class Solution {
     public int climbStairs(int n) {
         if(n == 0)
@@ -9,5 +10,30 @@ class Solution {
         int ans2 = climbStairs(n-2);
         
         return ans1+ans2;
+    }
+}
+
+// DP solution below
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    static int solve(int n, int dp[]){      
+        if(n == 0)
+            return 1;
+        if(n<0)
+            return 0;
+        
+        if(dp[n]!= -1){
+            return dp[n];
+        }else{
+            int ans1 = solve(n-1,dp);
+            int ans2 = solve(n-2,dp);
+            dp[n]=ans1+ans2;
+        }
+        
+        return dp[n];
     }
 }
